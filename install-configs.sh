11c14f185bfcc8ac6fd2b86579cea9a2a0079c42
Yuji Nelson <k.yuji.nelson@gmail.com>
2023-04-08 00:16:18 +0100

    feat: adds scripts for copying config files

---
+++ install-configs.sh
@@ -0,0 +1,4 @@
+# Copy settings over.
+sudo cp configs/com.apple.dock.plist ~/Library/Preferences
+sudo cp configs/com.apple.EmojiPreferences.plist ~/Library/Preferences
+sudo cp configs/.zshrc ~/
--- system-brew.sh
+++ system-brew.sh
@@ -3,6 +3,7 @@
 function brewSystem() {
   ./brew.sh;
   ./mas.sh;
+  ./install-configs.sh
 }
 
 read -p "This will install Homebrew and associated formulae, cask and some apps, plus some MAS apps. Ready? (y/n) " -n 1;
