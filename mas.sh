33c6412ce71b6d81feb6eeda0966c5b9949d39f2
Yuji Nelson <k.yuji.nelson@gmail.com>
2023-08-05 16:23:18 +0100

    feat: adds AmorphousDiskMark, Energize, and Hidden Bar to mas install list

--- mas.sh
+++ mas.sh
@@ -4,8 +4,11 @@
 # with `mas search <name>`.
 
 apps=(
+  1168254295 # AmorphousDiskMark https://apps.apple.com/us/app/amorphousdiskmark/id1168254295?mt=12
   937984704 # Amphetamine: https://apps.apple.com/us/app/amphetamine/id937984704?mt=12
+  1643751440 # Energiza https://apps.apple.com/us/app/energiza-battery-monitor/id1643751440?mt=12
   1423210932 # Flow - Focus & Pomodoro Timer: https://flowapp.info/
+  1452452150 # Hidden Bar https://apps.apple.com/us/developer/dwarves-foundation/id1452452150
   1440405750 # MusicHarbor: https://apps.apple.com/us/app/musicharbor-track-new-music/id1440405750
 )
 
