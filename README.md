4525c7cab0efe7320b4685cdd2a40f2e20748580
Yuji Nelson <k.yuji.nelson@gmail.com>
2023-08-12 21:52:45 +0100

    feat: refactors into a script that be run via curl

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A collection of scripts to run that aim to automate setting up a new Mac
 
-## Getting started
+## Usage
 
-Clone the repo and then run `system-brew.sh`
+To run the script direct you can use curl with the following command
+
+```shell
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/justaddcl/dotfiles/main/system-brew.sh)"
+```
--- system-brew.sh
+++ system-brew.sh
@@ -1,19 +1,376 @@
 #!/usr/bin/env bash
 
-# import util functions
-[ -f ./utils.sh ] && . ./utils.sh
+set -o errexit
+set -o pipefail
 
-function brewSystem() {
-  term_message cb "\nBrewing a new system."
-  ./brew.sh;
-  ./mas.sh;
-  ./install-configs.sh
-  term_message gb "\nSystem brew has completed."
+# Function to set terminal colors if supported.
+term_colors() {
+    if [[ -t 1 ]]; then
+        RED=$(printf '\033[31m')
+        GREEN=$(printf '\033[32m')
+        YELLOW=$(printf '\033[33m')
+        BLUE=$(printf '\033[34m')
+        MAGENTA=$(printf '\033[35m')
+        CYAN=$(printf '\033[36m')
+        BOLD=$(printf '\033[1m')
+        RESET=$(printf '\033[0m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        MAGENTA=""
+        CYAN=""
+        BOLD=""
+        RESET=""
+    fi
 }
 
-read -p "This will install Homebrew and associated formulae, cask and some apps, plus some MAS apps. Ready? (y/n) " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	brewSystem;
-fi;
-unset brewSystem;
+# Function to output colored or bold terminal messages.
+# Usage examples: term_message "This is a default color and style message"
+#                 term_message nb "This is a default color bold message"
+#                 term_message rb "This is a red bold message"
+term_message() {
+    local set_color=""
+    local set_style=""
+    [[ -z "${2}" ]] && echo -ne "${1}" >&2 && return
+    [[ ${1:0:1} == "d" ]] && set_color=${RESET}
+    [[ ${1:0:1} == "r" ]] && set_color=${RED}
+    [[ ${1:0:1} == "g" ]] && set_color=${GREEN}
+    [[ ${1:0:1} == "y" ]] && set_color=${YELLOW}
+    [[ ${1:0:1} == "b" ]] && set_color=${BLUE}
+    [[ ${1:0:1} == "m" ]] && set_color=${MAGENTA}
+    [[ ${1:0:1} == "c" ]] && set_color=${CYAN}
+    [[ ${1:1:2} == "b" ]] && set_style=${BOLD}
+    echo -e "${set_color}${set_style}${2}${RESET}" >&2 && return
+}
+
+# Displays a box containing a dash and message
+task_start() {
+    echo -ne "[-] ${1}"
+}
+
+# Displays a box containing a green tick and optional message if required.
+task_done() {
+    echo -e "\r[\033[0;32m\xE2\x9C\x94\033[0m] ${1}"
+}
+
+# Displays a box containing a red cross and optional message if required.
+task_fail() {
+    echo -e "\r[\033[0;31m\xe2\x9c\x98\033[0m] ${1}"
+}
+
+# Function to pause script and check if the user wishes to continue.
+check_continue() {
+    local response
+    while true; do
+        read -r -p "System brew will install Homebrew and associated formulae, cask, and some apps, MAS apps, and configs. Ready? (y/n) " response
+        case "${response}" in
+        [yY][eE][sS] | [yY])
+            echo
+            break
+            ;;
+        *)
+            echo
+            exit
+            ;;
+        esac
+    done
+}
+
+# Function check command exists
+command_exists() {
+    command -v "${@}" >/dev/null 2>&1
+}
+
+# Function to output details of script.
+script_info() {
+    cat <<EOF
+
+Name:           system-brew.sh
+Description:    Automate the installation of macOS
+                applications and packages using homebrew.
+                Fork of autobrew.sh by Mark Bradley
+Author:         JustAddCl
+Requirements:   Command Line Tools (CLT) for Xcode
+EOF
+}
+
+check_xcode() {
+    term_message cb "Checking for setup dependencies..."
+    task_start "Checking for Xcode command line tools..."
+    if xcode-select -p >/dev/null 2>&1; then
+        task_done "Xcode command line tools are installed.$(tput el)"
+    else
+        task_fail "\n"
+        term_message mb "Attempting to install Xcode command line tools..."
+        if xcode-select --install >/dev/null 2>&1; then
+            term_message gb "Re-run script after Xcode command line tools have finished installing.\n"
+        else
+            term_message rb "Xcode command line tools install failed.\n"
+        fi
+        exit 1
+    fi
+}
+
+install_homebrew() {
+    term_message cb "\nInstalling Homebrew..."
+    task_start "Checking for Homebrew..."
+    if command_exists "brew"; then
+        task_done "Homebrew is installed.$(tput el)"
+        task_start "Running brew update..."
+        if brew update >/dev/null 2>&1; then
+            task_done "Brew update completed.$(tput el)"
+        else
+            task_fail "Brew update failed.$(tput el)"
+        fi
+        task_start "Running brew upgrade..."
+        if brew upgrade >/dev/null 2>&1; then
+            task_done "Brew upgrade completed.$(tput el)"
+        else
+            task_fail "Brew upgrade failed.$(tput el)"
+        fi
+    else
+        task_fail "\n"
+        term_message mb "Attempting to install Homebrew..."
+        if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; then
+            task_done "Homebrew installed.\n"
+        else
+            task_fail "Homebrew install failed.\n"
+            exit 1
+        fi
+    fi
+}
+
+brew_packages() {
+    if [[ ! -z "$tap_list" ]]; then
+        term_message cb "\nAdding additional Homebrew taps..."
+        for tap in ${tap_list[@]}; do
+            task_start "Checking for tap > ${tap}"
+            if brew tap | grep "${tap}" >/dev/null 2>&1 || command_exists "${tap}"; then
+                task_done "Tap ${tap} already added.$(tput el)"
+            else
+                task_fail "\n"
+                term_message mb "Attempting to add tap ${tap}..."
+                if brew tap "${tap}"; then
+                    task_done "Tap ${tap} added.\n"
+                else
+                    task_fail "Unable to add tap ${tap}.\n"
+                fi
+            fi
+        done
+    fi
+    if [[ ! -z "$formulae_list" ]]; then
+        term_message cb "\nInstalling brew terminal packages..."
+        for formula in ${formulae_list[@]}; do
+            task_start "Checking for package > ${formula}"
+            if brew list "${formula}" >/dev/null 2>&1 || command_exists "${formula}"; then
+                task_done "Package ${formula} already installed.$(tput el)"
+            else
+                task_fail "\n"
+                term_message mb "Attempting to install ${formula}..."
+                if brew install "${formula}"; then
+                    task_done "Package ${formula} installed.\n"
+                else
+                    task_fail "Package ${formula} install failed.\n"
+                fi
+            fi
+        done
+    fi
+    if [[ ! -z "$cask_list" ]]; then
+        term_message cb "\nInstalling brew cask packages..."
+        for cask in ${cask_list[@]}; do
+            task_start "Checking for cask package > ${cask}"
+            if brew list --cask "${cask}" >/dev/null 2>&1; then
+                task_done "Package ${cask} already installed.$(tput el)"
+            else
+                task_fail "\n"
+                term_message mb "Attempting to install ${cask}..."
+                if brew install --cask "${cask}"; then
+                    task_done "Package ${cask} installed.\n"
+                else
+                    task_fail "Package ${cask} install failed.\n"
+                fi
+            fi
+        done
+    fi
+}
+
+brew_cleanup() {
+    task_start "Running brew cleanup..."
+    if brew cleanup >/dev/null 2>&1; then
+        task_done "Brew cleanup completed.$(tput el)"
+    else
+        task_fail "Brew cleanup failed.$(tput el)"
+    fi
+}
+
+install_zsh() {
+    task_start "Installing ZSH..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    task_done "ZSH installed.\n"
+}
+
+install_mas() {
+  task_message cb "\nInstalling Mac App Store apps..."
+  # Mac App Store apps have IDs. You can find these
+  # with `mas search <name>`.
+
+  apps=(
+    1168254295 # AmorphousDiskMark https://apps.apple.com/us/app/amorphousdiskmark/id1168254295?mt=12
+    937984704 # Amphetamine: https://apps.apple.com/us/app/amphetamine/id937984704?mt=12
+    1643751440 # Energiza https://apps.apple.com/us/app/energiza-battery-monitor/id1643751440?mt=12
+    1423210932 # Flow - Focus & Pomodoro Timer: https://flowapp.info/
+    1452452150 # Hidden Bar https://apps.apple.com/us/developer/dwarves-foundation/id1452452150
+    1440405750 # MusicHarbor: https://apps.apple.com/us/app/musicharbor-track-new-music/id1440405750
+  )
+
+  task_start "Installing apps..."
+
+  for app in "${apps[@]}"; do
+      mas install $app
+  done
+
+  task_done "Mac apps installed.\n"
+}
+
+install_configs() {
+  term_message cb "\nSetting up preferences..."
+
+  task_start "Downloading preferences..."
+  curl -o ~/Library/Preferences 'https://github.com/justaddcl/dotfiles/raw/main/configs/com.apple.dock.plist'
+  curl -o ~/Library/Preferences 'https://github.com/justaddcl/dotfiles/raw/main/configs/com.apple.EmojiPreferences.plist'
+  task_done "Preferences downloaded.\n"
+
+  task_start "Copying ZSH config..."
+  curl -o ~/ 'https://raw.githubusercontent.com/justaddcl/dotfiles/main/configs/.zshrc'
+  task_done "ZSH config copied."
+}
+
+install_lockscreen_image() {
+  term_message cb "\nInstalling lockscreen..."
+  desktop_pictures_dir='/Library/Caches/Desktop Pictures'
+
+  task_start "Getting User UUID..."
+  uuidWithFieldName=$(dscl . -read "/Users/$USER" GeneratedUID)
+  uuid=${uuidWithFieldName#'GeneratedUID: '}
+  task_done "User UUID retrieved. \n"
+
+  task_start "Checking for desktop pictures directories..."
+
+  # check if the /Library/Caches/Desktop Pictures directory exists
+  if [ -d "$desktop_pictures_dir/" ]; then
+
+    # check if there's a subfolder with the user's UUID
+    if [ -d "$desktop_pictures_dir/$uuid/" ]; then
+      task_done "Desktop pictures directories confirmed.\n"
+      task_start "Installing lockscreen image..."
+
+      # check if a lockscreen.png already exists
+      if [ -f "$desktop_pictures_dir/$uuid/lockscreen.png" ]; then
+          task_fail "Lockscreen image already exists in $desktop_pictures_dir/$uuid. Skipping copy. \n"
+        else
+
+          # check if repo has lockscreen to copy
+          if [ -f ./configs/lockscreen.png ]; then
+            cp "./configs/lockscreen.png" "$desktop_pictures_dir/$uuid/"
+            task_done "Lockscreen image copied into $desktop_pictures_dir/$uuid.\n"
+          else
+            task_fail "./configs/ does not have a lockscreen image to copy. \n"
+          fi
+      fi
+    else
+      task_fail "Desktop pictures directory for $uuid does not exist. \n"
+    fi
+  else
+      task_fail "Error: /Library/Caches/Desktop Pictures/ directory does not exist. \n"
+  fi
+}
+
+tap_list=(
+    homebrew/cask-fonts
+)
+
+formulae_list=(
+    bat
+    bat-extras
+    curl
+    eslint
+    gh
+    git
+    mas
+    node
+    nvm
+    prettier
+    ripgrep
+    tmux
+    typescript
+    wget
+    yarn
+)
+
+cask_list=(
+    1password
+    adobe-creative-cloud
+    appcleaner
+    arc
+    authy
+    brave-browser
+    coconutbattery
+    discord
+    disk-inventory-x
+    docker
+    figma
+    font-fira-code
+    font-fira-mono-for-powerline
+    firefox
+    fliqlo
+    flux
+    gifski
+    google-chrome
+    font-inconsolata-for-powerline
+    font-jetbrains-mono
+    kap
+    neovim
+    notion
+    paragon-ntfs
+    numi
+    postman
+    raycast
+    rectangle
+    rocket
+    signal
+    spotify
+    steam
+    thinkorswim
+    visual-studio-code
+    vlc
+    warp
+    webull
+    whatsapp
+)
+
+# One function to rule them all.
+main() {
+    # Customise the following list variables (tap_list, term_list and cask_list)
+    # Leave list blank or comment out the list if not required.
+    # tap_list="homebrew/cask-fonts"
+    # term_list="git htop wget curl tmux"
+    # cask_list="the-unarchiver vlc visual-studio-code google-chrome \
+    # firefox adobe-acrobat-reader malwarebytes font-fira-code"
+    clear
+    term_message cb "\nBrewing a new system..."
+    term_colors
+    script_info
+    check_continue
+    check_xcode
+    install_homebrew
+    brew_packages
+    brew_cleanup
+    install_zsh
+    install_mas
+    install_configs
+    term_message gb "\nSystem brew has completed."
+}
+
+main "${@}"
